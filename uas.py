87801ef62ea3115031442ff09c558294e8c545d4
YulianCandra <66151636+YulianCandra@users.noreply.github.com>
2020-06-02 10:54:15 +0700

    Tambahan pada data berat yang dipakai
    Data berat yang digunakan diambil dari dua cara. Pertama, menggunakan data berat original (kg). Kedua, menggunakan data berat dari hasil konversi volume benda menjadi berat (kg). Jika berat original lebih besar dari berat hasil konversi, maka data berat yang digunakan adalah berat original. Namun, jika berat original lebih kecil dari berat hasil konversi, maka data berat yang digunakan adalah berat hasil konversi.

--- uas.py
+++ uas.py
@@ -15,6 +15,13 @@ while iterasi == True:
     berat_paket= int(input('Berapa berat paket(dlm kg): '))
     jarak= int(input('Berapa jarak pengiriman: '))
     #durasi_kirim=int(input('Berapa lama paket sampai(dlm hari): '))
+    
+    berat_konversi= int((panjang_paket*lebar_paket*tinggi_paket)/(6000))
+    if berat_paket > berat_konversi:
+        berat= berat_paket
+    elif berat_paket < berat_konversi:
+        berat= berat_konversi
+    
     print('Pilihan Paket Pengiriman:')
     print('1. Yakin besok sampe: 1 hari pengiriman')
     print('2. Yakin lusa sampe: 2 hari pengiriman')
@@ -27,7 +34,7 @@ while iterasi == True:
         print('Alamat penerima:',alamat)
         print('biaya ongkir dasar= Rp ',paket_1,'per km' )
         biaya= paket_1*jarak
-        print ('\n','Jumlah ongkos kirim: Rp',(biaya*1)+biaya*((berat_paket-1)*0.7))
+        print ('\n','Jumlah ongkos kirim: Rp',(biaya*1)+biaya*((berat-1)*0.7))
     elif jp == '2':
         print('\n')
         print('Nama pengirim:',nama)
@@ -35,7 +42,7 @@ while iterasi == True:
         print('Alamat penerima:',alamat)
         print('biaya ongkir dasar= Rp ',paket_2,'per km' )
         biaya= paket_2*jarak
-        print ('\n','Jumlah ongkos kirim: Rp',(biaya*1)+biaya*((berat_paket-1)*0.7))
+        print ('\n','Jumlah ongkos kirim: Rp',(biaya*1)+biaya*((berat-1)*0.7))
     elif jp == '3':
         print('\n')
         print('Nama pengirim:',nama)
@@ -43,7 +50,7 @@ while iterasi == True:
         print('Alamat penerima:',alamat)
         print('biaya ongkir dasar= Rp ',paket_3,'per km' )
         biaya = paket_3*jarak
-        print ('\n','Jumlah ongkos kirim: Rp',(biaya*1)+biaya*((berat_paket-1)*0.7))
+        print ('\n','Jumlah ongkos kirim: Rp',(biaya*1)+biaya*((berat-1)*0.7))
     #print ('\n','Jumlah ongkos kirim: Rp',(biaya*1))
     #print ("Total transaksi hari ini: " + str(hitung),'\n')
     o_t= input('Ada traksaksi lain?(y/n) ')
